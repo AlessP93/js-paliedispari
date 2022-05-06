ff265aa49670ce39f719f0cf6664f22950f00195
AlessP93 <alespiscino@libero.it>
2022-05-06 15:30:22 +0200

    inizio palindroma

---
+++ main.js
@@ -0,0 +1,9 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+//1. chiedo all utente di inserire una parola
+const parola = prompt("Inserisci una parola")
+//2. creo una funzione per capire se la parola e palindroma
+function palindroma(parola) {
+    
+}
